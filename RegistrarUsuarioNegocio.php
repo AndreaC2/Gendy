df16e89e8b345f14d0f3a3ccfd2698da699f252b
AndreaC2 <62781166+AndreaC2@users.noreply.github.com>
2020-11-10 20:33:19 -0500

    Add files via upload

--- RegistrarUsuarioNegocio.php
+++ RegistrarUsuarioNegocio.php
@@ -28,13 +28,13 @@ try {
 <html>
     <head>
         <meta charset="utf-8">
-        <title>Gendy | Registro Cliente</title>
+        <title>Gendy | Registro Negocio</title>
         <link rel="stylesheet" href="css/registrar.css">
     </head>
     <body>
         <div class="registro_Cliente">
             <h2>Gendy</h2>
-            <h1>Registrar Cliente</h1>
+            <h1>Registrar Negocio</h1>
             <form method="post">
                 <label>Razon Social:</label>
                 <input type="text" name="razon">  
