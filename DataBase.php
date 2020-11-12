57500ad1fbf3a1b69e51276e42c8ed98a13dd872
Niser01 <64512516+Niser01@users.noreply.github.com>
2020-11-11 20:35:24 -0500

    Add files via upload

--- DataBase.php
+++ DataBase.php
@@ -3,12 +3,10 @@
 $conexion = 'mysql:host=localhost;port=3307;dbname=gendy';
 $usuario = 'root';
 $contraseña = 'root';
-	try{
+try{
     $DB = new PDO($conexion,$usuario,$contraseña);
     echo("connected");
-
-
-echo "<pre>\n";*/	}catch(PDOException $e){
+}catch(PDOException $e){
     echo("Error</br>");
     print $e->getMessage();
     die();
